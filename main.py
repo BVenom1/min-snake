b8788a2d68351df2930c45937cae01ac974f6eca
BVenom1 <bvenom169@gmail.com>
2025-09-22 12:05:34 +0300

    shortened function and variable names in html, wrote python code to generate version 40 qrcode with base64 encoding of html file

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import qrcode
 import base64
 import sys
+
+v = 40
 
 with open('index.html', 'r') as file:
     b64 = base64.b64encode(file.read().encode())
     b64_str = f'{b64}'.split("'")[1]
     print(sys.getsizeof(b64_str))
+    qr = qrcode.QRCode(version=v, box_size=5, border=10, error_correction=qrcode.ERROR_CORRECT_L)
+    qr.add_data(f'data:text/html;base64,{b64_str}')
+    qr.make(fit=True)
+    img = qr.make_image(fill_color = 'black', back_color = 'white')
+    img.save(f'min-sanke-{v}.png')
